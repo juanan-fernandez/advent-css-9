2d7f1618b205fdf28b34c91d167be999c4da894a
juanan fernandez <juanan.fernandez@gmail.com>
2023-02-09 20:51:14 +0100

    data config

---
+++ src/data/images-data.ts
@@ -0,0 +1,79 @@
+export const data = [
+	{
+		id: 1,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 2,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 3,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 4,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 5,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 6,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 7,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 8,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 9,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 10,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+	{
+		id: 11,
+		src: 'sherman-yang-VBBGigIuaDY-unsplash.jpg',
+		author: 'Sherman Yang',
+		gallery:
+			'https://unsplash.com/@emp_creative?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+	},
+];
